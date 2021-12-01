9c5a0a47b43b0ed1b0c5be1bfc2ad4e451c8b328
tmigot <tangi.migot@gmail.com>
2021-12-01 13:30:52 -0500

    add CI and everything

---
+++ docs/make.jl
@@ -0,0 +1,19 @@
+using ADNLPModels
+using Documenter
+using Printf
+using StoppingInterface
+
+makedocs(
+  modules = [StoppingInterface],
+  doctest = true,
+  # linkcheck = true,
+  strict = true,
+  format = Documenter.HTML(
+    assets = ["assets/style.css"],
+    prettyurls = get(ENV, "CI", nothing) == "true",
+  ),
+  sitename = "StoppingInterface",
+  pages = ["Home" => "index.md", "Tutorial" => "tutorial.md", "Reference" => "reference.md"],
+)
+
+deploydocs(repo = "github.com/tmigot/StoppingInterface.git", devbranch = "main")
