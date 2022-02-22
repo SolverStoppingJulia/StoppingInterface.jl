bb8900ba73856c43bc9e5b0cae379e7907ef0a22
tmigot <tangi.migot@gmail.com>
2022-02-22 13:52:49 -0500

    add docstrings

--- src/stopping_to_nlpmodels_ipopt.jl
+++ src/stopping_to_nlpmodels_ipopt.jl
@@ -1,8 +1,13 @@
 import NLPModelsIpopt: ipopt
 
 """
-ipopt(nlp) DOESN'T CHECK THE WRONG KWARGS, AND RETURN AN ERROR.
-ipopt(::NLPStopping)
+    `ipopt(stp::NLPStopping; subsolver_verbose::Int = 0, kwargs...)`
+
+Stopping-version of the `ipopt` function from NLPModelsIpopt.jl.
+This function calls `fill_in!` (doesn't update hessian) and `stop!` after `ipopt` call.
+
+`subsolver_verbose` corresponds to `print_level` argument in `ipopt`.
+Other keyword arguments are passed to the `ipopt` call.
 """
 function NLPModelsIpopt.ipopt(stp::NLPStopping; subsolver_verbose::Int = 0, kwargs...)
 
