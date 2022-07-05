490619415d0611b1989df2afaaa21deac45215be
tmigot <tangi.migot@gmail.com>
2022-07-05 19:41:05 +0200

    up to new KNITRO 13

--- src/stopping_to_nlpmodels_knitro.jl
+++ src/stopping_to_nlpmodels_knitro.jl
@@ -86,110 +86,107 @@ outlev: Controls the level of output produced by Knitro
         2 (iter_10) Print basic information every 10 iterations. (default in Knitro)
 =#
 
-@init begin
-  @require KNITRO = "67920dd8-b58e-52a8-8622-53c4cffbe346" begin
-    @require NLPModelsKnitro = "bec4dd0d-7755-52d5-9a02-22f0ffc7efcb" begin
-      is_knitro_installed = true
-      import NLPModelsKnitro: knitro
+if KNITRO.has_knitro()
+  import NLPModelsKnitro: knitro
 
-      """
-          `knitro(stp::NLPStopping; subsolver_verbose::Int = 0, kwargs...)`
+  """
+      `knitro(stp::NLPStopping; subsolver_verbose::Int = 0, kwargs...)`
 
-      Stopping-version of the `knitro` function from NLPModelsKnitro.jl.
-      This function doesn't calls `stop!` after `knitro` call.
-      Use the `KnitroSolver` structure to fill-in the gradient and objective in the state.
+  Stopping-version of the `knitro` function from NLPModelsKnitro.jl.
+  This function doesn't calls `stop!` after `knitro` call.
+  Use the `KnitroSolver` structure to fill-in the gradient and objective in the state.
 
-      `subsolver_verbose` corresponds to `print_level` argument in `knitro`.
-      Other keyword arguments are passed to the `knitro` call.
-      Selection of possible [options](https://www.artelys.com/docs/knitro/3_referenceManual/userOptions.html).
+  `subsolver_verbose` corresponds to `print_level` argument in `knitro`.
+  Other keyword arguments are passed to the `knitro` call.
+  Selection of possible [options](https://www.artelys.com/docs/knitro/3_referenceManual/userOptions.html).
 
-      It requires `using KNITRO, NLPModelsKnitro`.
-      """
-      function NLPModelsKnitro.knitro(
-        stp::NLPStopping;
-        convex::Int = -1, #let Knitro deal with it :)
-        objrange::Real = stp.meta.unbounded_threshold,
-        hessopt::Int = 1,
-        feastol::Real = stp.meta.rtol,
-        feastol_abs::Real = stp.meta.atol,
-        opttol::Real = stp.meta.rtol,
-        opttol_abs::Real = stp.meta.atol,
-        maxfevals::Int = min(stp.meta.max_cntrs[:neval_sum], typemax(Int32)),
-        maxit::Int = 0, #stp.meta.max_iter
-        maxtime_real::Real = stp.meta.max_time,
-        out_hints::Int = 0,
-        subsolver_verbose::Int = 0, #1 to see everything
-        algorithm::Int = 0, # *New* 2
-        ftol::Real = 1.0e-15, # *New*
-        ftol_iters::Int = 5, # *New*
-        xtol::Real = stp.meta.atol, # 1.0e-12, # *New*
-        xtol_iters::Int = 2, # 3 # *New*
-        kwargs...,
+  It requires `using KNITRO, NLPModelsKnitro`.
+  """
+  function NLPModelsKnitro.knitro(
+    stp::NLPStopping;
+    convex::Int = -1, #let Knitro deal with it :)
+    objrange::Real = stp.meta.unbounded_threshold,
+    hessopt::Int = 1,
+    feastol::Real = stp.meta.rtol,
+    feastol_abs::Real = stp.meta.atol,
+    opttol::Real = stp.meta.rtol,
+    opttol_abs::Real = stp.meta.atol,
+    maxfevals::Int = min(stp.meta.max_cntrs[:neval_sum], typemax(Int32)),
+    maxit::Int = 0, #stp.meta.max_iter
+    maxtime_real::Real = stp.meta.max_time,
+    out_hints::Int = 0,
+    subsolver_verbose::Int = 0, #1 to see everything
+    algorithm::Int = 0, # *New* 2
+    ftol::Real = 1.0e-15, # *New*
+    ftol_iters::Int = 5, # *New*
+    xtol::Real = stp.meta.atol, # 1.0e-12, # *New*
+    xtol_iters::Int = 2, # 3 # *New*
+    kwargs...,
+  )
+    @assert -1 ≤ convex ≤ 1
+    @assert 1 ≤ hessopt ≤ 7
+    @assert 0 ≤ out_hints ≤ 1
+    @assert 0 ≤ subsolver_verbose ≤ 6
+    @assert 0 ≤ maxit
+
+    stp.meta.start_time = time()
+    nlp = stp.pb
+    #y0 = stp.current_state.lambda #si défini
+    #z0 = stp.current_state.mu #si défini 
+    solver = NLPModelsKnitro.KnitroSolver(
+      nlp,
+      x0 = stp.current_state.x,
+      objrange = objrange,
+      feastol = feastol,
+      feastol_abs = feastol_abs,
+      opttol = opttol,
+      opttol_abs = opttol_abs,
+      maxfevals = maxfevals,
+      maxit = maxit,
+      maxtime_real = maxtime_real,
+      out_hints = out_hints,
+      algorithm = algorithm,
+      ftol = ftol,
+      ftol_iters = ftol_iters,
+      xtol = xtol,
+      xtol_iters = xtol_iters,
+      outlev = subsolver_verbose;
+      kwargs...,
+    )
+    stats = NLPModelsKnitro.knitro!(nlp, solver)
+    #@show stats.status, stats.solver_specific[:internal_msg]
+    #if stats.status ∉ (:unbounded, :exception, :unknown) #∈ (:first_order, :acceptable) 
+    stp.current_state.x = stats.solution
+    stp.current_state.fx = stats.objective
+    stp.current_state.gx = KNITRO.KN_get_objgrad_values(solver.kc)[2]
+    #norm(stp.current_state.gx, Inf)#stats.dual_feas #TODO: this is for unconstrained problem!!
+    stp.current_state.mu = stats.multipliers_L
+    if stp.pb.meta.ncon > 0
+      stp.current_state.cx = KNITRO.KN_get_con_values(solver.kc)
+      stp.current_state.Jx = sparse(
+        KNITRO.KN_get_jacobian_values(solver.kc)[1],
+        KNITRO.KN_get_jacobian_values(solver.kc)[2],
+        KNITRO.KN_get_jacobian_values(solver.kc)[3],
+        stp.pb.meta.ncon,
+        stp.pb.meta.nvar,
       )
-        @assert -1 ≤ convex ≤ 1
-        @assert 1 ≤ hessopt ≤ 7
-        @assert 0 ≤ out_hints ≤ 1
-        @assert 0 ≤ subsolver_verbose ≤ 6
-        @assert 0 ≤ maxit
-
-        stp.meta.start_time = time()
-        nlp = stp.pb
-        #y0 = stp.current_state.lambda #si défini
-        #z0 = stp.current_state.mu #si défini 
-        solver = NLPModelsKnitro.KnitroSolver(
-          nlp,
-          x0 = stp.current_state.x,
-          objrange = objrange,
-          feastol = feastol,
-          feastol_abs = feastol_abs,
-          opttol = opttol,
-          opttol_abs = opttol_abs,
-          maxfevals = maxfevals,
-          maxit = maxit,
-          maxtime_real = maxtime_real,
-          out_hints = out_hints,
-          algorithm = algorithm,
-          ftol = ftol,
-          ftol_iters = ftol_iters,
-          xtol = xtol,
-          xtol_iters = xtol_iters,
-          outlev = subsolver_verbose;
-          kwargs...,
-        )
-        stats = NLPModelsKnitro.knitro!(nlp, solver)
-        #@show stats.status, stats.solver_specific[:internal_msg]
-        #if stats.status ∉ (:unbounded, :exception, :unknown) #∈ (:first_order, :acceptable) 
-        stp.current_state.x = stats.solution
-        stp.current_state.fx = stats.objective
-        stp.current_state.gx = KNITRO.KN_get_objgrad_values(solver.kc)[2]
-        #norm(stp.current_state.gx, Inf)#stats.dual_feas #TODO: this is for unconstrained problem!!
-        stp.current_state.mu = stats.multipliers_L
-        if stp.pb.meta.ncon > 0
-          stp.current_state.cx = KNITRO.KN_get_con_values(solver.kc)
-          stp.current_state.Jx = sparse(
-            KNITRO.KN_get_jacobian_values(solver.kc)[1],
-            KNITRO.KN_get_jacobian_values(solver.kc)[2],
-            KNITRO.KN_get_jacobian_values(solver.kc)[3],
-            stp.pb.meta.ncon,
-            stp.pb.meta.nvar,
-          )
-          stp.current_state.lambda = stats.multipliers
-        end
-        stp.current_state.current_score = max(stats.dual_feas, stats.primal_feas)
-        #end
-        #Update the meta boolean with the output message
-        stp = stats_status_to_meta!(stp, stats)
-        #@show status(stp, list = true)
-        if status(stp) == :Unknown
-          @warn "Error in StoppingInterface statuses: return status is $(stats.status)"
-          #print(stats)
-        end
-
-        stp.meta.nb_of_stop = stats.iter
-        Stopping._update_time!(stp.current_state, time()) # stats.elapsed_time
-
-        return stp #would be better to return the stats somewhere
-      end
+      stp.current_state.lambda = stats.multipliers
     end
+    stp.current_state.current_score = max(stats.dual_feas, stats.primal_feas)
+    #end
+    #Update the meta boolean with the output message
+    stp = stats_status_to_meta!(stp, stats)
+    #@show status(stp, list = true)
+    if status(stp) == :Unknown
+      @warn "Error in StoppingInterface statuses: return status is $(stats.status)"
+      #print(stats)
+    end
+
+    stp.meta.nb_of_stop = stats.iter
+    Stopping._update_time!(stp.current_state, time()) # stats.elapsed_time
+
+    return stp #would be better to return the stats somewhere
   end
+else
+  @warn "Warning: KNITRO is not properly installed. See, KNITRO.jl."
 end
