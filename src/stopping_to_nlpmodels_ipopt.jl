c92cf9333e157096d36cbb9c1ab3ba0d0bea3d0c
tmigot <tmigot@users.noreply.github.com>
2022-08-19 00:57:46 +0000

    :robot: Format .jl files

--- src/stopping_to_nlpmodels_ipopt.jl
+++ src/stopping_to_nlpmodels_ipopt.jl
@@ -65,7 +65,7 @@ function NLPModelsIpopt.ipopt(
   success = stats.status ∈ [:first_order, :acceptable]
   if (success && fill_in_on_success) || (!success && fill_in_on_failure)
     x = stats.solution
-    λ = stats.multipliers 
+    λ = stats.multipliers
     μ = stats.multipliers_L - stats.multipliers_U
     # Not mandatory, but in case some entries of the State are used to stop
     fill_in!(stp, x, lambda = λ, mu = μ, Hx = stp.current_state.Hx)
